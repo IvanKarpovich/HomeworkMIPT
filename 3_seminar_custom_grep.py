d251721a09e9260c3a456b2b5dd1b35bf425b5e1
IvanKarpovich <saperoaudeo@gmail.com>
2022-11-03 18:59:45 +0300

    changes in 3_seminar_custom_grep

--- 3_seminar_custom_grep.py
+++ 3_seminar_custom_grep.py
@@ -1,11 +1,12 @@
 from typing import List
 
 def grep(pattern: str, file: str) -> List[str]:
-
+    found_lines = []
     with open(file, 'r', encoding='utf8') as f:
         for line in f:    
-            if line.find(pattern) != -1:
-                print( line )
+            if pattern in line:
+                found_lines.append(line)
+    return found_lines
 
 params = input().split()
 grep( *params )
